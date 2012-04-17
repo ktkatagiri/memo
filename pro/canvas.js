9da19dac7cf1e4997d28c2d22e08946312b35f32
Yuuhi <kafu.kitu.531@gmail.com>
2012-04-18 00:24:08 +0900

    hoge

--- pro/canvas.js
+++ pro/canvas.js
@@ -70,6 +70,58 @@ function drawMandelbrot(ctx){
     
 }
 
+// バーニングシップフラクタルの描画
+function drawBurningShip(ctx){
+    var a,b,x,y,zx,zy;
+    var Xtable = new Array();
+    var loop = 500;
+    var max = 500;
+
+    var med = max/2;
+
+    // ベスト？
+    var PosX = -0.033;
+    var PosY = - 1.755;
+    var Zoom = 0.079;
+
+    var colorMax = 0;
+
+    for( var i=0 ; i<max ; i++ )
+    {
+        a = PosY + Zoom * ( (i-med) / max );
+        Xtable[i] = new Array();
+        for( var j=0 ; j<max ; j++)
+        {
+            b = PosX + Zoom * ( (j-med) / max );
+            x=0;y=0;
+            for( var k=0 ; k<loop ; k++ )
+            {
+		x = Math.abs(x);
+		y = Math.abs(y);
+
+                zx = x*x-y*y+a;
+                zy = 2*x*y+b;
+                x = zx;
+                y = zy;
+                if( x*x+y*y>4 ) break;
+            }
+	    if(colorMax < k){ colorMax = k; }
+            Xtable[i][j] = k;
+        }
+    }
+
+    for(var i = 0; i < Xtable.length; i++) {
+	for(var j = 0; j < Xtable[i].length; j++) {
+	    ctx.beginPath();
+	    ctx.fillStyle = 'rgb(' + parseInt(255-Xtable[i][j]/colorMax * 255,10) + ',' + parseInt(255-Xtable[i][j]/colorMax * 255, 10) + ',' + parseInt(255-Xtable[i][j]/colorMax * 255,10) + ')';
+	    ctx.fillRect(i, j, 1, 1);
+	    ctx.fill();
+	}
+    }
+    
+}
+
+
 // コッホ曲線の描画
 // params, ctx: canvasの描画オブジェクト, p1: ポイント1, p2: ポイント2, n: 残りの再帰回数
 var draw = function(ctx, p1, p2, n) {
@@ -118,12 +170,18 @@ var draw = function(ctx, p1, p2, n) {
 
 // ここからはじまる
 var canvas_main = function(ctx) {
-    var p1 = {x: 10, y: 400};
-    var p2 = {x: 990, y: 400};
-    // // var p3 = {x: 250, y: 420};
+    // コッホ曲線
+    // var p1 = {x: 10, y: 400};
+    // var p2 = {x: 990, y: 400};
+    // // // var p3 = {x: 250, y: 420};
     // draw(ctx, p1, p2, 2);
     // draw(ctx, p2, p3, 5);
     // draw(ctx, p3, p1, n);
-    drawMandelbrot(ctx);
+
+    // マンデルブロ集合
+    // drawMandelbrot(ctx);
+
+    // バーニングシップフラクタル
+    drawBurningShip(ctx);
 };
 
